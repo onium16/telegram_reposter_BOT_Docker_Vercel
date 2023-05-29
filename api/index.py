5a00615275f4b1850c4944aa7ee66844697e9050
onium16 <onium16@gmail.com>
2023-05-29 09:42:35 +0200

    error index

--- api/index.py
+++ api/index.py
@@ -14,42 +14,16 @@ bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        if request.headers.get('content-type') == 'application/json':
-            json_data = request.get_json()
-            update = telebot.types.Update.de_json(json_data)
-            bot.process_new_updates([update])
-            return ''
-        else:
-            return Response(status=403)
+        json_data = request.get_json()
+        update = telebot.types.Update.de_json(json_data)
+        bot.process_new_updates([update])
+        return ''
+
         
-    elif request.method == 'GET':
-        return render_template("/webhook.html") 
-    else:
-        return "Неподдерживаемый метод запроса"
-
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template("/index.html") 
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    try:
-        variables = get_variables(message.from_user.id)
-        
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="Выберете необходимое действие")
-
-        bot.send_message(message.chat.id, """Бот готов к работе. Ниже последние настройки постинга изображений.
-        Воспользуйтесь кнопками меню для дальнейшей работы""", reply_markup=keyboard)
-        bot.send_message(message.chat.id, f'{variables}')
-    except:
-        bot.send_message(message.chat.id,'User settings not found.\nPlease enter request parameters for image reposting')
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="Выберете необходимое действие")
- 
-        bot.send_message(message.chat.id, """Бот готов к работе. Ниже последние настройки постинга изображений. 
-                        Воспользуйтесь кнопками меню для дальнейшей работы.""", reply_markup=keyboard)
-        bot.send_message(message.chat.id, f'{variables}')
