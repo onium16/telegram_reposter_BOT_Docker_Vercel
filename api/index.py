6b5953dbd88d7c8fd153a255aa7483bce596a343
onium16 <onium16@gmail.com>
2023-05-29 09:36:03 +0200

    index_test

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from flask import Flask, Response, render_template, request
 
 import telebot
 from telebot import types
-
+from api.main import get_variables
 
 
 TOKEN = os.environ.get('TOKEN')                      # for server app 
@@ -21,9 +21,7 @@ def webhook():
             json_data = request.get_json()
             update = telebot.types.Update.de_json(json_data)
             bot.process_new_updates([update])
-            @bot.message_handler(content_types=['text'])
-            def send_commands_not_supported(message, content_types = []):
-                return bot.send_message(message.chat.id, "Эта команда от бота /help")
+            return ''
         else:
             return Response(status=403)
         
@@ -37,7 +35,21 @@ def webhook():
 def index():
     return render_template("/index.html") 
 
-@bot.message_handler(content_types=['text'])
-def send_commands_not_supported(message, content_types = []):
-    bot.send_message(message.chat.id, "Эта команда не поддерживается ботом. Узнать список допустимых команд можно используя команду /help")
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    try:
+        variables = get_variables(message.from_user.id)
+        
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="Выберете необходимое действие")
+
+        bot.send_message(message.chat.id, """Бот готов к работе. Ниже последние настройки постинга изображений.
+        Воспользуйтесь кнопками меню для дальнейшей работы""", reply_markup=keyboard)
+        bot.send_message(message.chat.id, f'{variables}')
+    except:
+        bot.send_message(message.chat.id,'User settings not found.\nPlease enter request parameters for image reposting')
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, input_field_placeholder="Выберете необходимое действие")
+ 
+        bot.send_message(message.chat.id, """Бот готов к работе. Ниже последние настройки постинга изображений. 
+                        Воспользуйтесь кнопками меню для дальнейшей работы.""", reply_markup=keyboard)
+        bot.send_message(message.chat.id, f'{variables}')
